68c38c3c708571cc23c8e1abef64ab245217ee10
PROcsh <emtcsh@gmail.com>
2026-01-08 14:00:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "rescue-gps-v1";
+const CACHE_NAME = "gps-coord-cache-v1";
 
-const FILES = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
@@ -8,14 +8,31 @@ const FILES = [
   "https://cdnjs.cloudflare.com/ajax/libs/mgrs/1.0.0/mgrs.min.js"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES))
+// 설치
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>res||fetch(e.request))
+// 활성화
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// 요청 처리 (오프라인 우선)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
